453ece453b75d00b861f8a8ab39ac10312bb0473
Matthew Lee <matthew.lee711@gmail.com>
2016-10-19 23:26:19 -0500

    Added ChannelSection component - beginning App.js

--- components/channels/ChannelSection.jsx
+++ components/channels/ChannelSection.jsx
@@ -1,1 +1,22 @@
 import React, {Component} from 'react';
+import ChannelForm from './ChannelForm.jsx';
+import ChannelList from './ChannelList.jsx';
+
+var ChannelSection = createClass({
+	render() {
+		return(
+			<div>
+				<ChannelList {...this.props} />
+				<ChannelForm {...this.props} />
+			</div>
+		)
+	}
+});
+
+ChannelSection.propTypes = {
+	channels: React.Proptypes.array.isRequired,
+	addChannel: React.PropTypes.func.isRequired,
+	setChannel: React.PropTypes.func.isRequired
+}
+
+export default ChannelSection
