2f74ecffd28fe72e14fcaf586e92e9bf3a7d9404
Matthew Lee <matthew.lee711@gmail.com>
2016-10-23 02:07:26 -0500

    Created basic GoLang Websocket Server

--- go/main.go
+++ go/main.go
@@ -2,8 +2,15 @@ package main //make executable
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"net/http"
 )
+
+var upgrader = websocket.Upgrader {
+	ReadBufferSize: 	1024,
+	WriteBufferSize:	1024,
+	CheckOrigin:		func(r * http.Request) bool {return true},
+}
 
 func main() {
 	fmt.Println("Hello World")
@@ -12,5 +19,26 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
+	// fmt.Fprintf(w, "Hello!")
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//start reading messages from websocket
+	for {
+		msgType, msg, err := socket.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(msg))//msg is a byte array
+		//echo message back to client
+		//You can do an error checking if statement while assigning
+		//AND send message back to client
+		if err = socket.WriteMessage(msgType, msg); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
